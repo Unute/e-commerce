a38ce261355bcd0567a1714c1f99f5190789e9ad
Unute <jhfgjsk@gmail.com>
2026-03-01 17:26:55 +0300

    adaptiv

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import s from "./Header.module.scss";
 import BurgerMenu from "./components/BurgerMenu/BurgerMenu";
